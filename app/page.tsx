7ad44101bb0b33d740f778f1253a88f22a51289a
AzizkhonM <muzaffarovazizkhon@gmail.com>
2026-05-23 00:21:36 +0500

    first

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,28 @@
 import { cookies } from "next/headers";
 import { verifyToken } from "@/lib/jwt";
+import { JwtPayload } from "jsonwebtoken";
 
 export default async function Dashboard() {
-  const cookieStore = await cookies(); // ← await qo'shildi
+  const cookieStore = await cookies();
   const token = cookieStore.get("token")?.value;
 
   if (!token) {
     return <div>Not authorized</div>;
   }
 
-  const payload = verifyToken(token) as unknown as { userId: string } | null;
+  const payload = verifyToken(token);
 
   console.log(payload);
-  
 
   if (!payload) {
     return <div>Not authorized</div>;
   }
 
-  return <div>Welcome, user: {payload.userId}</div>;
+  if (typeof payload === "string") {
+    return <div>Invalid token</div>;
+  }
+
+  const userId = (payload as JwtPayload & { userId: string }).userId;
+
+  return <div>Welcome, user: {userId}</div>;
 }
